78e429cabcf88c9fe0f73ad491e9fb43b2bad587
Robert Lee Brackett III <robert.brackett3@gmail.com>
2014-09-30 15:56:47 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # Here is an example of the current_user method and helper that will make you layout view work
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
 end
